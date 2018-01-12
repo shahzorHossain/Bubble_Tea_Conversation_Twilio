9eea61b235beea22b0e4b6de2963ef9d2ab54852
shahzorhossain <shazorkhaled14@gmail.com>
2018-01-13 05:16:10 +0600

    correcting error'

--- config/keys.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  watsonAccountSID: "AC87d8a93e0b51ec3f88631dd2d09ec347",
-  watsonAuthToken: "2b1c57ab90b954fe708e2cd6d05fb7f4",
-  conversationUsername: "e098bee7-a5e4-440d-a27f-42f29735fba3",
-  conversationPassword: "mH0QsinvGON3",
-  conversation_workspace_id: "bd168f5d-8390-4199-b8f4-dc24300a5472"
-};
